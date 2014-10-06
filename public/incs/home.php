accdb256d39ce3eaa1fa98696d2c66bd70c5895b
Hebert Porto <hebertporto@gmail.com>
2014-10-06 07:45:22 -0300

    added twitter boostrap - estrutura basica da page - definicao dos diretorios

---
+++ public/incs/home.php
@@ -0,0 +1,5 @@
+<div class="jumbotron">
+    <h1>Design Patterns - Módulo 1<small> Gerar Formulário HTML </small></h1>
+    <p>This example is a quick exercise to illustrate how the default, static navbar and fixed to top navbar work.
+        It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
+</div>
---
+++ public/index.php
@@ -0,0 +1,32 @@
+<?php
+$rota = parse_url("http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+$url_final = substr($rota["path"], 1);
+
+if($url_final =='') # Traz a página Home ao acessar pela primeira vez.
+    $url_final = 'home';
+
+include('../autoload.php');
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Team Design Patterns Company</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/jRating.jquery.css" media="screen" />
+    <link rel="icon"  type="image/png" href="images/favicon.ico">
+</head>
+<body>
+<div class="container">
+    <?php require_once('incs/main_menu.php'); ?>
+    <?php require_once('incs/'. $url_final. '.php'); ?>
+</div>
+
+<script src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/jRating.jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/cliente.js"></script>
+</body>
+</html>
